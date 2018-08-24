63478d9d3145ddeb591d80e5c20a76a7e45ffab5
fumiphys <ufimihot2703@gmail.com>
2018-08-24 20:58:46 +0900

    template: add template file

---
+++ template/template.cpp
@@ -0,0 +1,30 @@
+#include <cstdio>
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <queue>
+#include <stack>
+#include <vector>
+#include <set>
+#include <map>
+#include <algorithm>
+#include <utility>
+#include <cmath>
+
+#define ll long long int
+#define pb push_back
+#define mk make_pair
+#define pq priority_queue
+
+using namespace std;
+typedef pair<int, int> P;
+typedef pair<ll, int> Pl;
+typedef pair<ll, ll> Pll;
+const int inf = 1e9;
+const ll linf = 1LL << 50;
+
+int main(int argc, char const* argv[])
+{
+	
+	return 0;
+}
