7a7a69352211a15500040a570a59628be3d6c678
fumiphys <ufimihot2703@gmail.com>
2018-10-14 18:51:14 +0900

    math: add function for calculating inverse

--- math/power.cpp
+++ math/power.cpp
@@ -24,14 +24,22 @@ int power(int a, int n, int mod) {
   return res;
 }
 
+// this function can be used when x and mod is coprime
+int inverse(int x, int mod) {
+  return power(x, mod - 2, mod);
+}
+
 
 int main(int argc, char const* argv[])
 {
-	cout << "-- test for dijkstra start --" << endl;
-  const int mod = 1e9;
+	cout << "-- test for power start --" << endl;
+  const int mod = 1e9 + 7;
   assert(power(2, 3, mod) == 8);
   assert(power(7, 4, mod) == 2401);
   assert(power(123, 5, mod) == (ll)28153056843 % mod);
-	cout << "-- test for dijkstra end --" << endl;
+
+  assert(inverse(1, mod) == 1);
+  assert(inverse(2, mod) == 5e8 + 4);
+	cout << "-- test for power end --" << endl;
   return 0;
 }
