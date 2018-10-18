30f591e1b0e77a378261a4bb74d4c73199b477ea
fumiphys <ufimihot2703@gmail.com>
2018-10-19 01:40:10 +0900

    graph: add max flow library Ford Fulkerson method

---
+++ graph/Ford_Fulkerson.hpp
@@ -0,0 +1,64 @@
+/*
+ * Ford Fulkerson algorithm
+ * author fumiphys
+ */
+#include <iostream>
+#include <vector>
+#include <utility>
+#include <algorithm>
+#include <climits>
+
+#define ll long long int
+#define pb push_back
+
+using namespace std;
+
+
+template <typename T>
+struct edge {int to; T cap; int rev;};
+
+template <typename T>
+struct Graph_ {
+  int n;
+  vector<bool> used;
+  vector<vector<edge<T>> > vec;
+  Graph_(int ns) {
+    n = ns;
+    used.resize(n);
+    vec.resize(n);
+  }
+  void adde(int at, int to, T cap){
+    vec[at].pb((edge<T>){to, cap, (int)vec[to].size()});
+    vec[to].pb((edge<T>){at, 0, (int)vec[at].size() - 1});
+  }
+  T dfs(int s, int t, int f){
+    if(s == t)return f;
+    used[s] = true;
+    for(int i = 0; i < vec[s].size(); i++){
+      edge<T> &e = vec[s][i];
+      if(!used[e.to] && e.cap > 0){
+        int d = dfs(e.to, t, min(f, e.cap));
+        if(d > 0){
+          e.cap -= d;
+          vec[e.to][e.rev].cap += d;
+          return d;
+        }
+      }
+    }
+    return 0;
+  }
+  T max_flow(int s, int t){
+    T inf = numeric_limits<T>::max();
+    T flow = 0;
+
+    while(true){
+      fill(used.begin(), used.end(), false);
+      int f = dfs(s, t, inf);
+      if(f == 0)return flow;
+      flow += f;
+    }
+  }
+};
+
+typedef Graph_<int> GraphI;
+typedef Graph_<ll> GraphL;
