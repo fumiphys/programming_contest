2a64e43e8cdce2ed541f7a77e98ffefa827f2a00
fumiphys <ufimihot2703@gmail.com>
2018-12-23 02:14:22 +0900

    utils: change to open terminal

--- utils/scripts/use_template.sh
+++ utils/scripts/use_template.sh
@@ -22,7 +22,7 @@ if [ -e ${FILENAME} ]; then
     case $OVERWRITE in
       "" | "Y" | "y" | "yes" | "Yes" | "YES" ) echo "file will be overwritten";;
       * ) echo "stop script!"
-          vim ${FILENAME}
+          vim ${FILENAME} -c "vert terminal" -c "wincmd r" -c "wincmd h"
           exit 0;;
     esac
 fi
@@ -34,4 +34,4 @@ mkdir -p test
 mkdir -p "test/test_${TNAME}"
 mkdir -p "test/test_${TNAME}/in"
 mkdir -p "test/test_${TNAME}/out"
-vim ${FILENAME}
+vim ${FILENAME} -c "vert terminal" -c "wincmd r" -c "wincmd h"
