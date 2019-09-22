7e2006bb979ee532666a252488ec0bbbe71e570a
fumiphys <ufimihot2703@gmail.com>
2019-09-22 18:10:59 +0900

    utils/procon_tools: remove pytest from requires

--- utils/procon_tools/setup.py
+++ utils/procon_tools/setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(name="procon_tools",
       version="0.0.0",
-      install_requires=["argparse", "beautifulsoup4", "termcolor", "pytest"],
+      install_requires=["argparse", "beautifulsoup4", "termcolor"],
       entry_points={"console_scripts": [
           "pc = app:main",
       ]})
