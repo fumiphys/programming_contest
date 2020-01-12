817de97972f6f47fe60793d4d155615ce10ac234
fumiphys <ufimihot2703@gmail.com>
2020-01-13 01:36:02 +0900

    [UPDATE] add random test to modint

--- math/src/modint.cpp
+++ math/src/modint.cpp
@@ -3,8 +3,11 @@
  */
 #include <iostream>
 #include <cassert>
+#include <random>
+#include "../algebra.hpp"
 #include "../modint.hpp"
 using namespace std;
+using ll = long long;
 
 int main(int argc, char const* argv[])
 {
@@ -21,6 +24,42 @@ int main(int argc, char const* argv[])
   assert(m2 / m1 == Mint(3));
   m = Mint(2);
   assert(m.inverse() == Mint(int(5e8+4)));
+
+  const long long MOD = 998244353;
+  random_device rnd;
+  mt19937 mt(rnd());
+  uniform_int_distribution<> dist(0, 10);
+  auto gen = [&]() -> ll {
+    ll g = dist(mt);
+    return g - 5;
+  };
+  vector<ll> v;
+  int n = 100000;
+  ll curr = 0;
+  Mint mcurr(0, MOD);
+  for(int i = 0; i < n; i++){
+    ll v = gen();
+    int op = dist(mt) % 4;
+    if(op == 0){
+      curr = (curr + v) % MOD;
+      if(curr < 0)curr += MOD;
+      mcurr = mcurr + Mint(v, MOD);
+    }else if(op == 1){
+      curr = (curr - v) % MOD;
+      if(curr < 0)curr += MOD;
+      mcurr = mcurr - Mint(v, MOD);
+    }else if(op == 2){
+      curr = (curr * v) % MOD;
+      if(curr < 0)curr += MOD;
+      mcurr = mcurr * Mint(v, MOD);
+    }else{
+      if(v == 0)continue;
+      curr = (curr * modinv<ll>(abs(v), MOD) * (v > 0 ? 1: -1)) % MOD;
+      if(curr < 0)curr += MOD;
+      mcurr = mcurr / Mint(v, MOD);
+    }
+    assert(mcurr.x == curr);
+  }
   cout << "-- test for modint end: Success --" << endl;
   return 0;
 }
