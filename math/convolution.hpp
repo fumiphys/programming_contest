965f3c1704bff8780fd7d1bd3a92bf7608af8f96
fumiphys <ufimihot2703@gmail.com>
2020-02-04 03:22:48 +0900

    [UPDATE] fix type in FFT

--- math/convolution.hpp
+++ math/convolution.hpp
@@ -56,7 +56,8 @@ custom_complex<T> pol(T r, T theta){
   return custom_complex<T>(r * cosl(theta), r * sinl(theta));
 }
 
-using cd = custom_complex<long double>;
+using dbase = long double;
+using cd = custom_complex<dbase>;
 // f.size() should be the power of 2.
 void rec_fft(vector<cd> &f, bool inv=false){
   int n = f.size();
@@ -68,7 +69,7 @@ void rec_fft(vector<cd> &f, bool inv=false){
   }
   rec_fft(f0, inv);
   rec_fft(f1, inv);
-  double th = m_pi * 2 / n;
+  dbase th = m_pi * 2 / n;
   if(inv)th = - th;
   cd u(cos(th), sin(th)), ui(1., 0);
   for(int i = 0; i < n; i++){
@@ -82,7 +83,7 @@ void fft(vector<cd> &f, bool inv=false){
   int n = f.size(), mask = n - 1;
   vector<cd> tmp(n);
   for(int i = n >> 1; i >= 1; i >>= 1){
-    cd zeta = pol<long double>(1., 2. * m_pi * i * (inv ? -1.: 1.) / n);
+    cd zeta = pol<dbase>(1., 2. * m_pi * i * (inv ? -1.: 1.) / n);
     cd w = cd(1., 0);
     for(int j = 0; j < n; j += i){
       for(int k = 0; k < i; k++){
